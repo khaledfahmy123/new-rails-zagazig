b74eed91d9c3d27dccd83d70fe7b93285cefc936
Khaled Fahmy <khaledfahmy345@gmail.com>
2026-06-06 15:53:14 +0300

    refactor: posts full crud ops

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,56 @@
 class PostsController < ApplicationController
-  # your code goes here
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/:id
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :content ])
+  end
 end
